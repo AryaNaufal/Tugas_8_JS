65b3605d6e479661cd7b6a4462a1e9c34fde39e9
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-01 23:53:50 +0700

    TUGAS 8 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function obj(){
+    var mobil= {
+        harga: 1000000,
+        jenis: "sedan",
+        merk: "kijang"
+    }
+    mobil.merk = "inova";
+    
+    console.log(mobil)
+    console.log(mobil.jenis)
+}
+
+obj()
